8466715355f6a0d43908573e08e678697015e8f2
Kapil-coder <soni.kapil789@gmail.com>
2026-02-11 16:31:18 +0530

    REFRESH TOKEN INJECTED

--- client/api/apiClient.js
+++ client/api/apiClient.js
@@ -6,23 +6,68 @@ const apiClient = axios.create({
     "Content-Type": "application/json",
   },
   timeout: 10000,
+  withCredentials:true
 })
 
-// later auth add here
+
 apiClient.interceptors.request.use((config) => {
   return config;
 });
 
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const message =
-      error.response?.data?.message ||
-      error.message ||
-      "Something went wrong";
 
-    return Promise.reject(new Error(message));
-  }
-);
+// REFRESH CONTROL VARIABLE
+let isRefresh = false
+let failedQue=[]
+
+const processQueue=(error)=>{
+  failedQue.forEach(({resolve,reject})=>{
+    error? reject(error): resolve()
+  });
+  failedQue=[]
+}
+
+// RESPONSE INTERCEPTOR
+apiClient.interceptors.response.use(
+  (response)=>response,
+  
+  async (error)=>{
+    const originalRequest =error.config
+
+     // access token expired -> try refresh
+     if(
+      error.response?.status===401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/refresh")
+    ){
+      originalRequest._retry = true;
+
+      // if Refresh already in process then wait
+      if(isRefresh){
+        return new Promise((resolve, reject) => {
+          failedQue.push({resolve,reject})
+        }).then(()=>apiClient(originalRequest))
+      }
+
+       isRefresh = true;
+
+        try {
+        //  refresh token call
+        await apiClient.post("/auth/refresh");
+
+        // Retry all waiting
+        processQueue(null);
+
+        return apiClient(originalRequest);
+      } catch (err) {
+        processQueue(err);
+        return Promise.reject(err);
+      } finally {
+        isRefresh = false;
+      }
+    }
+      return Promise.reject(error);
+    
+  } 
+)
 
 export default apiClient;
