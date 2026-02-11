7db365aa3fbc2a3cdde73789c90cc8d586365555
Kapil-coder <soni.kapil789@gmail.com>
2026-02-11 16:45:32 +0530

    Auth connected with frontend

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -1,39 +1,73 @@
-import React from 'react'
-import Input from "../components/comman/Input"
-import Button from '../components/comman/Button'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from "react";
+import Input from "../components/comman/Input";
+import Button from "../components/comman/Button";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../api/auth.api";
 
 function Login() {
-  const navigate=useNavigate()
-  const handleLogin=(e)=>{
-    e.preventDefault()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    navigate("/dashboard")
+  const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("")
+    
+    try{
+      const res = await loginUser({email,password})
+       console.log("login success:", res.data);
 
-  }  
+      navigate("/dashboard");
+    }catch(err){
+      setError(err.message || "Login failed");
+    }
+
+    
+  };
   return (
-    
-        <div className="login-content">
-            <div className="login-heading">
-                <h1>Welcome back</h1>
-                <p>Log in to manage your shared expenses</p>
-            </div>
+    <div className="login-content">
+      <div className="login-heading">
+        <h1>Welcome back</h1>
+        <p>Log in to manage your shared expenses</p>
+      </div>
 
-            <form onSubmit={handleLogin} className="form-fields">
-                <Input id="password" type='email' label="Email" placeholder='Enter your email'/>
-                <Input id="password" type='password' label="Password" placeholder='Enter your password'/>
-               
-               <Button text='Log in' type='submit' variant='primary' className='auth-submit'/>
-            </form>
+      <form onSubmit={handleLogin} className="form-fields">
+        <Input
+          id="email"
+          type="email"
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          id="password"
+          type="password"
+          label="Password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p className="auth-error">{error}</p>}
 
-            <p className='auth-switch'>
-                Don't have an accont? <span><Link to="/signup" >Create one</Link></span>
-            </p>
-        </div>
-    
-  )
+        <Button
+          text="Log in"
+          type="submit"
+          variant="primary"
+          className="auth-submit"
+        />
+      </form>
+
+      <p className="auth-switch">
+        Don't have an account?{" "}
+        <span>
+          <Link to="/signup">Create one</Link>
+        </span>
+      </p>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/auth/Signup.jsx
+++ client/src/auth/Signup.jsx
@@ -1,32 +1,79 @@
-import React from 'react'
-import Input from '../components/comman/Input'
-import Button from '../components/comman/Button'
-import {Link} from "react-router-dom"
+import React from "react";
+import Input from "../components/comman/Input";
+import Button from "../components/comman/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../../api/auth.api";
 
 function Signup() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const res = await registerUser({ name, email, password });
+      console.log("signup success:", res.data);
+
+      navigate("/login");
+    } catch (err) {
+      setError(err.message || "Signup failed");
+    }
+  };
   return (
     <div className="login-content">
-            <div className="login-heading">
-                <h1>Create your account</h1>
-                <p>Start splitting expenses with ease</p>
-            </div>
+      <div className="login-heading">
+        <h1>Create your account</h1>
+        <p>Start splitting expenses with ease</p>
+      </div>
 
-            <form action="" className="form-fields">
-                <Input id="name" type='text' label="Full Name" placeholder='Enter your name'/>
+      <form onSubmit={handleSignup} className="form-fields">
+        <Input
+          id="name"
+          type="text"
+          label="Full Name"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
+        <Input
+          id="email"
+          type="email"
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          id="password"
+          type="password"
+          label="Password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p className="auth-error">{error}</p>}
+        <Button
+          text="Create Accont"
+          variant="primary"
+          className="auth-submit"
+        />
+      </form>
 
-                <Input id="email" type='email' label="Email" placeholder='Enter your email'/> 
-                <Input id="password" type='password' label="Password" placeholder='Enter your password'/>
-               
-               <Button text='Create Accont' variant='primary' className='auth-submit'/>
-            </form>
-
-            <p className='auth-switch'>
-                Already have an accont? <span><Link to="/login">Login</Link></span>
-            </p>
-        </div>
-   
-  )
+      <p className="auth-switch">
+        Already have an account?{" "}
+        <span>
+          <Link to="/login">Login</Link>
+        </span>
+      </p>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
