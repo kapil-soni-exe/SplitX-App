d099c05a98eb5772510031c0f15cc5c4f17f1859
Kapil-coder <soni.kapil789@gmail.com>
2026-02-11 17:04:28 +0530

    CORS Update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,13 @@ require("./models/group.model");
 require("./models/expense.model");
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser())
 
 app.use("/api/groups", groupRoutes);
