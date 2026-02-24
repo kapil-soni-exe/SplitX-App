4203b4c78cef6236e0d75b0564e519cc55c0e2a3
Kapil-coder <soni.kapil789@gmail.com>
2026-02-24 15:18:19 +0530

    Groups routes added

--- Backend/src/routes/group.routes.js
+++ Backend/src/routes/group.routes.js
@@ -2,13 +2,16 @@ const express = require("express")
 const router = express.Router()
 const Protected = require("../../middleware/auth.middleware")
 
-const {createGroup,getAllgroup} = require("../controller/group.controller")
+const {createGroup,getAllgroup,getGroupbyId} = require("../controller/group.controller")
 
 // Create Group
 router.post("/",Protected,createGroup)
 
 // FetchAllGroup
 router.get("/",Protected,getAllgroup)
+
+// Fetch Group by Id
+router.get("/:groupId",Protected,getGroupbyId)
 
 
 
