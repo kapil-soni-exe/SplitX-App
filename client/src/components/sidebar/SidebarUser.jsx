a24d09d4b117d39ce0ee67d08480a48484da8c41
Kapil-coder <soni.kapil789@gmail.com>
2026-02-25 11:15:44 +0530

    Sidebar Logout Working

--- client/src/components/sidebar/SidebarUser.jsx
+++ client/src/components/sidebar/SidebarUser.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import Button from "../comman/Button"
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function SidebarUser() {
+  const {logout} = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogout= async()=>{
+   await logout()
+   navigate("/login");
+  }
+
   return (
     <div className="sidebar-user">
         <div className="user-info">
@@ -13,7 +23,7 @@ function SidebarUser() {
                 <p className="user-email">Kapil@email.com</p>
             </div>
         </div>
-      <Button text='Log out'  variant='danger' className='logout-btn'/>
+      <Button text='Log out' onClick={handleLogout}  variant='danger' className='logout-btn'/>
         
     </div>
   )
