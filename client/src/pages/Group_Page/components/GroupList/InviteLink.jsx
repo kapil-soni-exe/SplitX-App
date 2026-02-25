6790342c351497ce4d35314ed902cfc23dc7764c
Kapil-coder <soni.kapil789@gmail.com>
2026-02-25 11:12:16 +0530

    Invite Link Shareable

--- client/src/pages/Group_Page/components/GroupList/InviteLink.jsx
+++ client/src/pages/Group_Page/components/GroupList/InviteLink.jsx
@@ -1,22 +1,37 @@
 import Button from "../../../../components/comman/Button";
+import { RiShareLine } from "@remixicon/react";
 
+function InviteSuccess({
+  inviteLink,
+  title = "Invite members",
+  subtitle = "Share invite link with others",
+}) {
+  const handleShare = async () => {
+    try {
+      if (navigator.share) {
+        await navigator.share({
+          title: "Join my SplitX group",
+          text: "Join our group to split expenses easily",
+          url: inviteLink,
+        });
+      } else {
+        await navigator.clipboard.writeText(inviteLink);
+        alert("Invite link copied");
+      }
+    } catch (err) {
+      console.error("Share failed", err);
+    }
+  };
 
-function InviteSuccess({ inviteLink}) {
   return (
     <div className="invite-success">
-      <h3>Group Created 🎉</h3>
-      <p>Share this link:</p>
+      <h3>{title}</h3>
+      <p>{subtitle}</p>
 
-      <div
-        className="invite-box"
-        onClick={() => navigator.clipboard.writeText(inviteLink)}
-      >
-        {inviteLink}
-      </div>
-
-      <p className="copy-hint">Click to copy</p>
-
-      
+      <Button onClick={handleShare}>
+        <RiShareLine size={18} />
+        Share invite link
+      </Button>
     </div>
   );
 }
