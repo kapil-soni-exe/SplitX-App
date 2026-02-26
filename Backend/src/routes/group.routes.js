8654dda43f5e22bba3b72ffebe87ea1bdc5d65d3
Kapil-coder <soni.kapil789@gmail.com>
2026-02-26 20:25:53 +0530

    Group Routes change

--- Backend/src/routes/group.routes.js
+++ Backend/src/routes/group.routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const Protected = require("../../middleware/auth.middleware")
 
-const {createGroup,getAllgroup,getGroupbyId} = require("../controller/group.controller")
+const {createGroup,getAllgroup,getGroupbyId, CheckInviteCode, joinGroup} = require("../controller/group.controller")
 
 // Create Group
 router.post("/",Protected,createGroup)
@@ -13,6 +13,11 @@ router.get("/",Protected,getAllgroup)
 // Fetch Group by Id
 router.get("/:groupId",Protected,getGroupbyId)
 
+// Check Invite Code
+router.get("/invite/:inviteCode",CheckInviteCode)
+
+// JoinGroupByInviteCode
+router.post("/join/:inviteCode",Protected,joinGroup)
 
 
 
