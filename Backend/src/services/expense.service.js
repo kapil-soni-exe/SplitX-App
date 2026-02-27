19fa8edb7aad9acc6f9c49620a8088a81c36a39e
Kapil-coder <soni.kapil789@gmail.com>
2026-02-27 16:02:02 +0530

    Expense feature fixed

--- Backend/src/services/expense.service.js
+++ Backend/src/services/expense.service.js
@@ -1,8 +1,10 @@
 const Expense = require("../models/expense.model");
+const Group = require("../models/group.model")
+const User = require("../models/users.model")
 const { calculateEqualSplit,calculateExactSplit } = require("../../utils/splitCalculator")
 
 const createExpense = async (data, user) => {
-  const { splitType, amount, splitBetween, splits, paidBy } = data;
+  const { splitType, amount, splitBetween, splits, paidBy,groupId } = data;
 
   let finalSplits = [];
 
@@ -14,11 +16,37 @@ const createExpense = async (data, user) => {
     finalSplits = calculateExactSplit({ amount, splits, paidBy });
   }
 
-  return Expense.create({
+  //  Expense create
+  const expense = await Expense.create({
     ...data,
     splits: finalSplits,
     createdBy: user._id,
   });
+
+  const paidByUser = await User.findById(expense.paidBy).select("name");
+ 
+ // Update Group Last Expense (FIXED)
+await Group.findByIdAndUpdate(groupId, {
+  lastExpense: {
+    expenseId: expense._id,
+    title: expense.title,
+    amount: expense.amount,
+
+    paidBy: {
+       _id: paidByUser._id,
+        name: paidByUser.name,
+    },
+
+    createdBy: {
+      _id: user._id,
+      name: user.name,
+    },
+
+    createdAt: expense.createdAt,
+  },
+});
+
+  return expense;
 };
 
 module.exports = { createExpense };
