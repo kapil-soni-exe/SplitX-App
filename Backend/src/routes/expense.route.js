ef583d773daafb904de0bdecf018a5abab6fbf7f
Kapil-coder <soni.kapil789@gmail.com>
2026-03-02 17:36:29 +0530

    edit/delete route added

--- Backend/src/routes/expense.route.js
+++ Backend/src/routes/expense.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createExpense,getExpensesByGroup } = require("../controller/expense.controller");
+const { createExpense,getExpensesByGroup, updateExpense, deleteExpenseController } = require("../controller/expense.controller");
 const protected = require("../../middleware/auth.middleware")
 
 // Create Expense
@@ -8,5 +8,13 @@ router.post("/", protected,createExpense);
 
 // fetch expenses by group
 router.get("/", protected, getExpensesByGroup);
+
+// Edit Expenses
+router.patch("/:expenseId",protected,updateExpense)
+
+// Delete Expense
+router.delete("/:expenseId",protected,deleteExpenseController)
+
+
 
 module.exports = router;
