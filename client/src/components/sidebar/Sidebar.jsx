2f4b2b728f9e6582abc378c579683b1a8bb77675
Kapil-coder <soni.kapil789@gmail.com>
2026-03-03 22:01:34 +0530

    Some Changes

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -2,29 +2,50 @@ import React from "react";
 import "./sidebar.css";
 import SidebarItem from "./SidebarItem";
 import SidebarUser from "./SidebarUser";
-import { RiExchange2Line, RiLayout2Line,RiMoneyRupeeCircleLine,RiTeamLine } from "@remixicon/react";
-
+import { 
+  RiLayout2Line,
+  RiTeamLine,
+  RiUser3Line 
+} from "@remixicon/react";
 
 const navItems = [
-  { label: "Dashboard", path: "/dashboard",icon: <RiLayout2Line className="sidebar-icon" /> },
-  { label: "Groups", path: "/groups", icon: <RiTeamLine className="sidebar-icon" />  },
-  { label: "Expenses", path: "/expenses",icon: <RiMoneyRupeeCircleLine className="sidebar-icon" /> },
-  { label: "Settlements", path: "/settlements",icon: <RiExchange2Line className="sidebar-icon" /> },
+  { 
+    label: "Dashboard", 
+    path: "/dashboard",
+    icon: <RiLayout2Line className="sidebar-icon" /> 
+  },
+  { 
+    label: "Groups", 
+    path: "/groups",
+    icon: <RiTeamLine className="sidebar-icon" />  
+  },
+  { 
+    label: "Profile", 
+    path: "/profile",
+    icon: <RiUser3Line className="sidebar-icon" />  
+  },
 ];
 
 function Sidebar() {
   return (
-    <aside className="sidebar" >
+    <aside className="sidebar">
       <div className="sidebar-logo">
         <img src="/logo1.png" alt="auth-log" className='logo light' />
-                <img src="/logo-light.png" alt="auth-log" className='logo dark' />
+        <img src="/logo-light.png" alt="auth-log" className='logo dark' />
       </div>
+
       <nav className="sidebar-nav">
-        {navItems.map((item, idx) => {
-          return <SidebarItem key={idx} icon={item.icon} path={item.path} label={item.label} />;
-        })}
+        {navItems.map((item, idx) => (
+          <SidebarItem 
+            key={idx}
+            icon={item.icon}
+            path={item.path}
+            label={item.label}
+          />
+        ))}
       </nav>
-      <SidebarUser/>
+
+      <SidebarUser />
     </aside>
   );
 }
