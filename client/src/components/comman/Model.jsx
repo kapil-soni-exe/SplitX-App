af38ee35610d93c3d22f5a5690b99ea263e7219d
Kapil-coder <soni.kapil789@gmail.com>
2026-03-03 22:03:01 +0530

    Now model always open (Body)

--- client/src/components/comman/Model.jsx
+++ client/src/components/comman/Model.jsx
@@ -1,17 +1,47 @@
-import "./Model.css"
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import { RiCloseCircleLine } from "@remixicon/react";
+import "./Model.css";
 
-import {RiCloseCircleLine} from "@remixicon/react"
-function Modal({ isOpen, onClose, children }) {
+function Modal({ isOpen, onClose, children, variant = "center" }) {
+  // Prevent background scroll
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
-  return (
-    <div className="modal-backdrop">
-      <div className="modal-box">
-        <RiCloseCircleLine className="close-btn" onClick={onClose}/>
-        {children}
-      </div>
-    </div>
+  return createPortal(
+    <div className="modal-backdrop" onClick={onClose}>
+      {variant === "center" && (
+        <div
+          className="modal-box"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <RiCloseCircleLine
+            className="close-btn"
+            onClick={onClose}
+          />
+          {children}
+        </div>
+      )}
+
+      {variant === "drawer" && (
+        <div onClick={(e) => e.stopPropagation()}>
+          {children}
+        </div>
+      )}
+    </div>,
+    document.body
   );
 }
 
-export default Modal
+export default Modal;
