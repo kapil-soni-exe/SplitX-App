dba2381c5d55eae183d707536542d450afc2a809
Kapil-coder <soni.kapil789@gmail.com>
2026-03-04 22:02:33 +0530

    Added leave endpoint

--- Backend/src/routes/group.routes.js
+++ Backend/src/routes/group.routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const Protected = require("../../middleware/auth.middleware")
 
-const {createGroup,getAllgroup,getGroupbyId, CheckInviteCode, joinGroup} = require("../controller/group.controller")
+const {createGroup,getAllgroup,getGroupbyId, CheckInviteCode, joinGroup, leaveGroup} = require("../controller/group.controller")
 
 // Create Group
 router.post("/",Protected,createGroup)
@@ -18,6 +18,9 @@ router.get("/invite/:inviteCode",CheckInviteCode)
 
 // JoinGroupByInviteCode
 router.post("/join/:inviteCode",Protected,joinGroup)
+
+// Leave The Group
+router.post("/:groupId/leave", Protected, leaveGroup);
 
 
 
