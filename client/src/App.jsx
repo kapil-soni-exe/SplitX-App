182d23204b2e059b41682f964aaf24124ab208bd
Kapil-coder <soni.kapil789@gmail.com>
2026-03-05 23:11:07 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,9 @@
-import React from 'react'
-import {RouterProvider} from "react-router-dom"
-import { router } from './routes'
-
-
-
-
-
-
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
-  return (
-    
-    <RouterProvider router={router}/>
-
-  )
+  return <RouterProvider router={router} />;
 }
 
-
-
-export default App
+export default App;
