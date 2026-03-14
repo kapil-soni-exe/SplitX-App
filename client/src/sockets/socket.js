239fa5d8acff1d134d040daabd2307c9e60e0f90
Kapil-coder <soni.kapil789@gmail.com>
2026-03-14 22:12:35 +0530

    Socket fix

--- client/src/sockets/socket.js
+++ client/src/sockets/socket.js
@@ -1,12 +1,28 @@
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 
-const socket = io("http://localhost:3000",{
-   withCredentials: true
+const socket = io(import.meta.env.VITE_API_URL?.replace("/api", "") || "http://localhost:3000", {
+  withCredentials: true,
+  autoConnect: false,        // don't connect until user is logged in
 })
 
 socket.on("connect", () => {
-  console.log(socket.id); 
+  console.log("[Socket] Connected:", socket.id);
 });
 
+socket.on("disconnect", (reason) => {
+  console.log("[Socket] Disconnected:", reason);
+});
 
-export default socket;
+socket.on("connect_error", (err) => {
+  console.warn("[Socket] Connection error:", err.message);
+});
+
+export const connectSocket = () => {
+  if (!socket.connected) socket.connect();
+};
+
+export const disconnectSocket = () => {
+  if (socket.connected) socket.disconnect();
+};
+
+export default socket;
