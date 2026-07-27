18daf0ae36a336e105b075b8131836e405de9ab9
Kapil-coder <soni.kapil789@gmail.com>
2026-07-27 23:14:21 +0530

    loading styling added

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,24 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Spinner from "./Loaders/Spinner"
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
   const location = useLocation();
 
   if (loading) {
-    return <div>Checking session...</div>;
-  }
+  return (
+    <div style={{ 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent: "center", 
+      height: "100vh",
+      background: "var(--bg-main)"
+    }}>
+      <Spinner/>
+    </div>
+  );
+}
 
   if (!user) {
     return (
